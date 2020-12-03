8dd06e9955d9c7f1910cbdb4a905e2428bae279d
GoldenCodex <evan.norman@me.com>
2020-12-03 09:33:34 -0700

    new presence update

--- Commands/hug.js
+++ Commands/hug.js
@@ -2,11 +2,11 @@ module.exports = {
     name: 'hug',
     decription: 'Give a hug to a user',
     usage: '-hug <@user>',
-    cooldown: 5,
+    cooldown: 15,
     execute(message, args) {
         let target = message.mentions.users.first();
         if (!target) return message.reply('You must mention a user to  hug');
-        if (target == message.author) return message.channel.send(`${message.author} hugged themself. how sad.`);
+        if (target == message.author) return message.channel.send(`${message.author} hugged themself. how sad :(`);
         message.channel.send(`${message.author} gave ${target} a hug!`);
     }
 }
--- index.js
+++ index.js
@@ -16,13 +16,27 @@ const {
 client.once('ready', () => {
     console.log(chalk.bgGreen("INFO:") + (` Bot has started with ${client.users.cache.size} users, in ${client.channels.cache.size} channels, of ${client.guilds.cache.size} guilds`));
 });
-
+/*
 client.on('ready', () => {
     client.user.setActivity('Flame Development', { type: 'WATCHING' })
         .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
         .catch(console.error);
 });
+*/
 
+const activities_list = [
+    "Flame Development", 
+    "-help for help",
+    "https://github.com/goldentg/Flame", 
+    `Currently in ${client.guilds.cache.size} guilds`
+    ]; // creates an arraylist containing phrases you want your bot to switch through.
+
+client.on('ready', () => {
+    setInterval(() => {
+        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
+        client.user.setActivity(activities_list[index], { type: 'WATCHING'}); // sets bot's activities to one of the phrases in the arraylist.
+    }, 10000); // Runs this every 10 seconds.
+});
 
 client.commands = new Discord.Collection();
 
