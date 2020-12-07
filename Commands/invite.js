b4f136c662e949e2a5cacaa0f141e17cd89ebd76
BlackLung <goldentgcoding@gmail.com>
2020-12-06 18:06:38 -0700

    added support server to invite command

--- Commands/invite.js
+++ Commands/invite.js
@@ -1,6 +1,7 @@
 module.exports = {
 name: 'invite',
 execute(message) {
-message.channel.send('invite Flame to your server using this link: https://discord.com/api/oauth2/authorize?client_id=783796609405812777&permissions=8&scope=bot');
+message.channel.send('invite Flame to your server using this link: https://discord.com/api/oauth2/authorize?client_id=783796609405812777&permissions=8&scope=bot
+You can also join the bot support server here: https://discord.gg/GZ3xSkd');
 }
 }
