c8327179362f8967fd43cd0c722245fb538226b5
GoldenCodex <evan.norman@me.com>
2020-12-07 19:24:20 -0700

    moved invite into an embed

--- Commands/say.js
+++ Commands/say.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'say',
     desription: 'requires administrative permissions',
     execute(message, args) {
-        if (!message.member.hasPermission('ADMINISTRATOR')) return message.reply('You do not have the permissions to do this');
+        if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('You do not have the permissions to do this');
         if (!args[0]) return message.reply('You must say something for the bot to say');
         const words = args.join(" ");
         message.channel.send(words)
