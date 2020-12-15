85fa5b265e0e82f1ae30ea14f17e709ac6237f69
GoldenCodex <evan.norman@me.com>
2020-12-15 08:41:04 -0700

    bobross command

--- Commands/bobross.js
+++ Commands/bobross.js
@@ -1,6 +1,7 @@
 module.exports = {
     name: 'bobross',
     definition: 'sends a random bob ross episode',
+    cooldown: 10,
     execute(message) {
         message.channel.send('enjoy a random episode of The Joy of Painting <https://mityurl.com/y/cDIn/r>');
     }
